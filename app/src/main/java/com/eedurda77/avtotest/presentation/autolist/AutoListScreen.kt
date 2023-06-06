c16910b2090942be0b9f98441d975905d7b8c505
HayarovEd <edurd@rambler.ru>
2023-06-06 15:33:16 +0500

    show error in AutoListScreen

--- app/src/main/java/com/eedurda77/avtotest/presentation/autolist/AutoListScreen.kt
+++ app/src/main/java/com/eedurda77/avtotest/presentation/autolist/AutoListScreen.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @SuppressLint("StateFlowValueCalledInComposition")
@@ -23,6 +25,22 @@ fun AutoListScreen(
 ) {
     val state = viewModel.state.collectAsState()
     LazyColumn(modifier = modifier.fillMaxSize()) {
+        if (state.value.error!=null) {
+            item{
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        modifier = modifier,
+                        text = state.value.error?:"Unknown error",
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
         items(state.value.items.size) { number ->
             val auto = state.value.items[number]
             if (number >= state.value.items.size - 1 && !state.value.endReached && !state.value.isLoading) {
