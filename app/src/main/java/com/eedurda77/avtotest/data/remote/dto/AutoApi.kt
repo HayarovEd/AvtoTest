81fac133342a70c77c0d60fda85f6064e7110b98
HayarovEd <edurd@rambler.ru>
2023-06-06 11:16:20 +0500

    add autoApi, DiModule

---
+++ app/src/main/java/com/eedurda77/avtotest/data/remote/dto/AutoApi.kt
@@ -0,0 +1,28 @@
+package com.eedurda77.avtotest.data.remote.dto
+
+import com.eedurda77.avtotest.data.remote.dto.autoinfo.AutoInfoDto
+import com.eedurda77.avtotest.data.remote.dto.autos.AutoDtoItem
+import com.eedurda77.avtotest.data.remote.dto.post.PostDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface AutoApi {
+    @GET("cars/list")
+    suspend fun getAutos (
+        @Query("page") page: Int,
+        @Query("items") itemsInPage: Int
+    ) : List<AutoDtoItem>
+
+    @GET("car/{id}")
+    suspend fun getAutoInfo (
+        @Path("id") autoId: Int
+    ) : AutoInfoDto
+
+    @GET("car/{id}")
+    suspend fun getPosts (
+        @Path("id") autoId: Int,
+        @Query("page") page: Int,
+        @Query("items") itemsInPage: Int
+    ) : PostDto
+}
