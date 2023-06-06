59bab0c7c411ee7561ce2bedce6793078afde5a0
HayarovEd <edurd@rambler.ru>
2023-06-06 20:16:19 +0300

    correct AutoInfoViewModel - gat all data

--- app/src/main/java/com/eedurda77/avtotest/presentation/autoinfo/AutoInfoState.kt
+++ app/src/main/java/com/eedurda77/avtotest/presentation/autoinfo/AutoInfoState.kt
@@ -1,12 +1,12 @@
 package com.eedurda77.avtotest.presentation.autoinfo
 
-import com.eedurda77.avtotest.data.remote.dto.post.Post
 import com.eedurda77.avtotest.domain.model.AutoInfo
+import com.eedurda77.avtotest.domain.model.PostAuto
 
 data class AutoInfoState(
     val isLoading: Boolean = false,
     val auto: AutoInfo? = null,
-    val items: List<Post> = emptyList(),
+    val items: List<PostAuto> = emptyList(),
     val error: String? = null,
     val endReached: Boolean = false,
     val page: Int = 1
--- app/src/main/java/com/eedurda77/avtotest/presentation/autoinfo/AutoInfoViewModel.kt
+++ app/src/main/java/com/eedurda77/avtotest/presentation/autoinfo/AutoInfoViewModel.kt
@@ -3,30 +3,60 @@ package com.eedurda77.avtotest.presentation.autoinfo
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.eedurda77.avtotest.data.paginator.PaginatorDefault
 import com.eedurda77.avtotest.domain.repository.RepositoryAuto
 import com.eedurda77.avtotest.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class AutoInfoViewModel @Inject constructor(
     private val repositoryAuto: RepositoryAuto,
-    private val savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle
 ): ViewModel() {
     private var _state = MutableStateFlow(AutoInfoState())
     val state = _state.asStateFlow()
 
+    val id = savedStateHandle.get<String>("id") ?: "0"
+    val paginator = PaginatorDefault(
+        initialKey = _state.value.page,
+        onLoadUpdated = {
+            _state.value = _state.value.copy(isLoading = it)
+        },
+        onRequest = { nextPage ->
+            repositoryAuto.getPostByAuto(
+                autoId = id.toInt(),
+                page = nextPage,
+                pageSize = 10
+            )
+        },
+        getNextKey = {
+            _state.value.page + 1
+        },
+        onError = {
+            _state.value = _state.value.copy(error = it)
+        },
+        onSuccess = { items, newKey ->
+            _state.value = _state.value.copy(
+                items = _state.value.items + items,
+                page = newKey,
+                endReached = items.isEmpty()
+            )
+        }
+    )
+
     init {
         getRemoteData()
+        loadNextItems()
     }
 
     private fun getRemoteData() {
         viewModelScope.launch {
-            val id = savedStateHandle.get<String>("id") ?: return@launch
+
             when (val result = repositoryAuto.getAutoInfoById(id = id.toInt())) {
                 is Resource.Error -> {
                     _state.update { currentState ->
@@ -35,6 +65,7 @@ class AutoInfoViewModel @Inject constructor(
                         )
                     }
                 }
+
                 is Resource.Success -> {
                     _state.update { currentState ->
                         currentState.copy(
@@ -45,4 +76,12 @@ class AutoInfoViewModel @Inject constructor(
             }
         }
     }
+
+
+    fun loadNextItems() {
+        viewModelScope.launch {
+            paginator.loadNextItems()
+        }
+    }
+
 }
