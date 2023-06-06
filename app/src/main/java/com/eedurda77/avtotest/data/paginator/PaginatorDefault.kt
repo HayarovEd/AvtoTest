46cbb692d3e0108157de8f1a5940610263c3b8d1
HayarovEd <edurd@rambler.ru>
2023-06-06 13:52:32 +0500

    add AutoListViewModel, correct paginator

--- app/src/main/java/com/eedurda77/avtotest/data/paginator/PaginatorDefault.kt
+++ app/src/main/java/com/eedurda77/avtotest/data/paginator/PaginatorDefault.kt
@@ -1,14 +1,15 @@
 package com.eedurda77.avtotest.data.paginator
 
 import com.eedurda77.avtotest.domain.paginator.Paginator
+import com.eedurda77.avtotest.utils.Resource
 import javax.inject.Inject
 
 class PaginatorDefault<Key, Item> @Inject constructor(
     initialKey: Key,
     private inline val onLoadUpdated: (Boolean) -> Unit,
-    private inline val onRequest: suspend (nextKey: Key) -> Result<List<Item>>,
+    private inline val onRequest: suspend (nextKey: Key) -> Resource<List<Item>>,
     private inline val getNextKey: suspend (List<Item>) -> Key,
-    private inline val onError: suspend (Throwable?) -> Unit,
+    private inline val onError: suspend (String?) -> Unit,
     private inline val onSuccess: suspend (items: List<Item>, newKey: Key) -> Unit
 ) : Paginator<Key, Item> {
     private var currentKey = initialKey
@@ -21,13 +22,20 @@ class PaginatorDefault<Key, Item> @Inject constructor(
         onLoadUpdated(true)
         val result = onRequest(currentKey)
         isMakingRequest = false
-        val items = result.getOrElse {
-            onError(it)
-            onLoadUpdated(false)
-            return
+        when (result) {
+            is Resource.Error -> {
+                onError(result.message)
+                onLoadUpdated(false)
+            }
+            is Resource.Success -> {
+                result.data.let {items->
+                    if (items != null) {
+                        currentKey = getNextKey(items)
+                        onSuccess(items, currentKey)
+                    }
+                    onLoadUpdated(false)
+                }
+            }
         }
-        currentKey = getNextKey(items)
-        onSuccess(items, currentKey)
-        onLoadUpdated(false)
     }
 }
